6aae3c94b320f240b72cbf00805d9bd1f414ab41
JorgeAndres21 <jorgeaguevaram@gmail.com>
2023-04-01 20:23:23 -0300

    adicionado o finally

--- index.js
+++ index.js
@@ -24,12 +24,19 @@ app.get('/users', (request, response) => {
 })
 
 app.post('/users', (request, response) => {
+    try {
     const { name, age } = request.body
     const user = { id: uuid.v4(), name, age }
+        if(age < 18) throw new Error('so de maiores')
 
     users.push(user)
 
     return response.status(201).json(user)
+} catch (err) {
+    return response.status(500).json({Message:err.message})
+} finally{
+    console.log('Terminou tudo')
+}
 })
 
 app.put('/users/:id', requestId , (request, response) => {
